b24a6a8bbfa2491984a54e8286a3ed04dca32591
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-10 17:12:05 -0600

    sign up form created

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 'use strict';
 let app = angular.module('LivePolling', ['ngRoute']);
 
-// app.config(($routeProvider) => {
-//   $routeProvider
-//   .when('/', {
-//     templateUrl: 'views/test.html'
-//   });
-// });
+app.config(($routeProvider) => {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/home.html'
+  })
+  .when('/login', {
+    templateUrl: 'views/login.html'
+  });
+});
