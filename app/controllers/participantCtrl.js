1703743d8706137418b077b7f22d89442f2ccb45
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-14 07:15:32 -0600

    parsing data via actionCable

--- app/controllers/participantCtrl.js
+++ app/controllers/participantCtrl.js
@@ -3,31 +3,41 @@
 module.exports = function(
   $scope,
   $routeParams,
-  presentationFactory
+  presentationFactory,
+  api
 ) {
 
-  //this is temporary to be replaced by slide_count column in db
+  let ActionCable = require('../../lib/node_modules/actioncable/lib/assets/compiled/action_cable.js');
+
 
   $scope.currentPresentation = {};
 
+  // const showPresentation = () => {
+  //   presentationFactory.showToParticipant($routeParams.presentationId)
+  //   .then(data => {
+  //     if (data.message) {
+  //       $scope.currentPresentation.message = data.message;
+  //       $scope.currentPresentation.title = data.title;
+  //     } else {
+  //       $scope.currentPresentation = data.presentation;
+  //     }
+  //   })
+  //   .catch(error => console.log(error));
+  // };
+
   const showPresentation = () => {
-    presentationFactory.showToParticipant($routeParams.presentationId)
-    .then(data => {
-      console.log(data);
-      if (data.message) {
-        $scope.currentPresentation.message = data.message;
-        $scope.currentPresentation.title = data.title;
-      } else {
-        $scope.currentPresentation = data.presentation;
+    let cable = ActionCable.createConsumer(api.ws);
+
+    cable.subscriptions.create('PresentationChannel', {
+      received: (data) => {
+        $scope.currentPresentation = data;
+        console.log('web socket data?', $scope.currentPresentation);
       }
-    })
-    .catch(error => console.log(error));
+    });
   };
 
   $scope.$on('$viewContentLoaded', () => {
     showPresentation();
-    let action = ActionCable.new();
-    console.log('action?', action);
   });
 
 };
--- app/creds/api.js
+++ app/creds/api.js
@@ -3,7 +3,8 @@
 module.exports = {
   url: 'http://localhost:3000',
   // url: 'https://ancient-sierra-59888.herokuapp.com',
-
+  ws: 'ws://localhost:3000/cable',
+  // ws: 'wss://ancient-sierra-59888.herokuapp.com/cable',
   userModel: '/users',
   userLogIn: '/authenticate',
 
