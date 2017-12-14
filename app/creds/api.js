877a40f36b9fac7cd93bc9f03514693dd64efdfe
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-14 10:03:16 -0600

    changed api location to test remote

--- app/creds/api.js
+++ app/creds/api.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'http://localhost:3000',
-  // url: 'https://ancient-sierra-59888.herokuapp.com',
-  ws: 'ws://localhost:3000/cable',
-  // ws: 'wss://ancient-sierra-59888.herokuapp.com/cable',
+  // url: 'http://localhost:3000',
+  url: 'https://ancient-sierra-59888.herokuapp.com',
+  // ws: 'ws://localhost:3000/cable',
+  ws: 'wss://ancient-sierra-59888.herokuapp.com/cable',
   userModel: '/users',
   userLogIn: '/authenticate',
 
