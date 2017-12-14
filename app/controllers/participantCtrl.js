1a6abe65f1c724a7e43e934bc4ab618258a23a02
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-14 15:19:04 -0600

    participant view rendering

--- app/controllers/participantCtrl.js
+++ app/controllers/participantCtrl.js
@@ -3,18 +3,21 @@
 module.exports = function(
   $scope,
   $routeParams,
+  $timeout,
   presentationFactory,
   api
 ) {
 
   let ActionCable = require('../../lib/node_modules/actioncable/lib/assets/compiled/action_cable.js');
 
-
+  $scope.title = '';
   $scope.currentPresentation = {};
 
   const showPresentation = () => {
     presentationFactory.showToParticipant($routeParams.presentationId)
-    .then(() => {
+    .then((data) => {
+      $scope.title = data.presentation.title;
+      console.log($scope.currentPresentation);
       let cable = ActionCable.createConsumer(api.ws);
       cable.subscriptions.create({
         channel: 'PresentationChannel',
@@ -23,6 +26,9 @@ module.exports = function(
         received: (data) => {
           $scope.currentPresentation = data;
           console.log('web socket data?', $scope.currentPresentation);
+          console.log('web socket data 2?', $scope.currentPresentation.current_poll);
+          console.log('web socket data 3?', $scope.currentPresentation.items);
+          $timeout();
         }
       });
     })
