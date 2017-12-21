12d5d3c349f73c9bdd57acc041fd4f10c005ee84
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-21 10:51:32 -0600

    displays error messages with bad signup and login

--- app/controllers/userCtrl.js
+++ app/controllers/userCtrl.js
@@ -1,26 +1,31 @@
 'use strict';
 
-module.exports = function($scope, $window, userFactory) {
+module.exports = function($scope, $window, userFactory, stringService) {
 
   // values filled for easy login,
   // delete values on deploy
   //creates model for holding form data for creating new user
   $scope.user = {
-    firstName: 'Jeremy',
-    lastName: 'Wells',
-    email: 'example@example.com',
-    password: '123456',
-    passwordConfirmation: '123456'
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
   };
 
   $scope.loginError = false;
+  $scope.errorTypes = [];
+  $scope.errorVals = [];
 
   // posts user data to db endpoint post 'signup' 'users#create'
   // calls login() function on return to generate auth token for user
   $scope.signUp = () => {
     userFactory.signUp($scope.user)
     .then(response => $scope.logIn())
-    .catch(error => console.log(error));
+    .catch(error => {
+      $scope.loginError = true;
+      renderErrors(error.data);
+    });
   };
 
   // takes email and password from user model
@@ -39,6 +44,15 @@ module.exports = function($scope, $window, userFactory) {
     userFactory.logOut()
     .then(() => $window.location.href = '/')
     .catch((error => console.log(error)));
+  };
+
+
+  // takes data from sign up errors
+  // creates errorType and errorValues arrays, removes underscores in names
+  const renderErrors = (errorData) => {
+    let rawErrorTypes = Object.keys(errorData);
+    $scope.errorTypes = rawErrorTypes.map(string => stringService.removeUnderscores(string));
+    $scope.errorVals = Object.values(errorData);
   };
 
 };
