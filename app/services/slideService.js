bcb67d25c92b83e4f8b9430d06334fc812982fa2
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-01-04 16:42:37 -0600

    removed console logs

--- app/services/slideService.js
+++ app/services/slideService.js
@@ -7,7 +7,6 @@ module.exports = function(
   // takes desired slide number, id of presentatin to modify, user token, and optional redirect url
   // resets slide number of current presentation
   const setSlideNumber = (slideNumber, presentationId, token, url) => {
-    console.log('slide number', slideNumber);
     let presentationObj = {presentation: {current_slide: slideNumber}};
     presentationFactory.editPresentation(presentationObj, presentationId, token)
     .then(() => {
