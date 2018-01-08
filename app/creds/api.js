c9ba076ea55a33bf80daa8252c66f54e6fed134a
Jeremy Wells <jsheridanwells@gmail.com>
2018-01-08 13:56:46 -0600

    Update api.js

--- app/creds/api.js
+++ app/creds/api.js
@@ -4,13 +4,10 @@
 // holds all uris and endpoints for api calls and websocket connections
 // commenting can toggle development and production urls
 module.exports = {
-  // clientUrl: 'http://localhost:8080/#!',
-  clientUrl: 'http://jeremywells.io/live-polling/#!',
+  clientUrl: 'http://localhost:8080/#!',
   show: '/show',
-  // url: 'http://localhost:3000',
-  url: 'https://ancient-sierra-59888.herokuapp.com',
-  // ws: 'ws://localhost:3000/cable',
-  ws: 'wss://ancient-sierra-59888.herokuapp.com/cable',
+  url: 'http://localhost:3000',
+  ws: 'ws://localhost:3000/cable',
   userModel: '/users',
   userLogIn: '/authenticate',
   presentations: '/presentations',
